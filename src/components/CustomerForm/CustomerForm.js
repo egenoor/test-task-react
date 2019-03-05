874d3c3aa18c10dc03ed690f90472b521c148f96
Ege-Rita Noor <egenoor@users.noreply.github.com>
2019-03-05 16:46:37 +0200

    add validation to form fields

--- src/components/CustomerForm/CustomerForm.js
+++ src/components/CustomerForm/CustomerForm.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import SimpleReactValidator from 'simple-react-validator'
+import moment from 'moment'
 import './CustomerForm.scss'
 
 class CustomerForm extends Component {
@@ -11,6 +13,7 @@ class CustomerForm extends Component {
       username: '',
       password: ''
     }
+    this.validator = new SimpleReactValidator()
     this.handleFormValues = this.getFormValues.bind(this)
     this.handleAddCustomer = this.addCustomer.bind(this)
   }
@@ -18,29 +21,39 @@ class CustomerForm extends Component {
   getFormValues(e) {
     let stateCopy = JSON.parse(JSON.stringify(this.state))
     stateCopy[e.target.id] = e.target.value
+    if(e.target.id === 'dateOfBirth'){
+      const date = moment(e.target.value, 'YYYY-MM-DD')
+      stateCopy[e.target.id] = date
+      console.log(date.isValid())
+    }
     this.setState(stateCopy)
   }
 
   addCustomer(e) {
     e.preventDefault()
-    fetch('/api/addCustomer', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(this.state)
-    })
-      .then(res => {
-        this.setState(this.state)
-        this.props.getAllCustomers()
-        this.props.handleClose()
+    if (this.validator.allValid()) {
+      fetch('/api/addCustomer', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(this.state)
       })
-      .catch(err => {
-        console.log(err)
-      })
+        .then(res => {
+          this.setState(this.state)
+          this.props.getAllCustomers()
+          this.props.handleClose()
+        })
+        .catch(err => {
+          console.log(err)
+        })      
+    } else {
+      this.validator.showMessages();
+      this.forceUpdate();
+    }
+
   }
-  
 
   render () {
     return (
@@ -49,22 +62,27 @@ class CustomerForm extends Component {
           <label htmlFor="firstName">
             First name
             <input id="firstName" onChange={this.handleFormValues} type="text"></input>
+            {this.validator.message('firstName', this.state.firstName, 'required|alpha')}
           </label>
           <label htmlFor="lastName">
             Last name
             <input id="lastName" onChange={this.handleFormValues} type="text"></input>
+            {this.validator.message('lastName', this.state.lastName, 'required|alpha')}
           </label>
           <label htmlFor="dateOfBirth">
             Date of birth
             <input id="dateOfBirth" onChange={this.handleFormValues} type="date"></input>
+            {this.validator.message('dateOfBirth', this.state.dateOfBirth, 'required|date')}
           </label>
           <label htmlFor="username">
             Username
             <input id="username" onChange={this.handleFormValues} type="text"></input>
+            {this.validator.message('username', this.state.username, 'required')}
           </label>
           <label htmlFor="password">
             Password
             <input id="password" onChange={this.handleFormValues} type="password"></input>
+            {this.validator.message('password', this.state.password, 'required|alpha_num_dash|min:8|max:32')}
           </label>
           <input type="submit" value="Add"></input>
         </form>
